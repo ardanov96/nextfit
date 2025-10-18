d515211d11ab5aacc1e366f9a8934b7cf6ef1c47
Ardha Putra <ardhakrs@gmail.com>
2025-10-18 11:26:44 +0800

    update about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,73 @@
+"use client";
+
+import {FaUsers} from 'react-icons/fa';
+import {IoIosPricetags} from 'react-icons/io';
+import {FaDumbbell} from 'react-icons/fa6';
+
+import {motion} from 'framer-motion';
+import {fadeIn} from '@/lib/variants';
+
+const featured = [
+    {
+        icon: <FaUsers />,
+        title: 'award-winning trainers',
+        subtitle: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti tempore 
+            quibusdam suscipit, nulla consectetur eos dignissimos porro sapiente ipsam.`,
+    },
+    {
+        icon: <IoIosPricetags />,
+        title: 'excellent prices',
+        subtitle: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti tempore 
+            quibusdam suscipit, nulla consectetur eos dignissimos porro sapiente ipsam.`,
+    },
+    {
+        icon: <FaDumbbell />,
+        title: 'modern equipment',
+        subtitle: 
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti tempore 
+            quibusdam suscipit, nulla consectetur eos dignissimos porro sapiente ipsam.`,
+    },
+];
+
 const About = () => {
     return (
-        <section className='h-screen bg-yellow-300' id='about'>
-            About
+        <section className='pt-8 pb-14 lg:pt-16 lg:pb-28' id='about'>
+            <div className="container mx-auto">
+               <div className='flex flex-col items-center gap-2 mb-8'>
+                  <h2 className='h2 text-center'>About Us</h2>
+                    <p className='max-w-[600px] mx-auto text-center'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minus perferendis atque voluptas? Nesciunt, quo velit?
+                    </p>
+               </div>
+            </div>
+            {/* featured items*/}
+            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16'>
+                {featured.map((feature, index) => {
+                    return (
+                        <div 
+                            className='flex flex-col justify-center items-center gap-4 border p-10'
+                            key={index}
+                        >
+                            <div className='text-4xl bg-primary-300 text-white w-[80px] h-[80px]
+                            rounded-full flex justify-center items-center'>
+                                {feature.icon}
+                            </div>
+                            <div className='flex flex-col justify-center items-center
+                            gap-2 text-center'>
+                                <h4 className='h4 text-accent'>{feature.title}</h4>
+                                <p>{feature.subtitle}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            
         </section>
     );
 };
+
 export default About;
+  
--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -18,19 +18,71 @@ const HeroSlider = () => {
             <SwiperSlide>
                 <div className='h-full flex justify-end pt-48'>
                     <div className='flex flex-col items-center lg:items-start lg:max-w-[850px]'>
-                        <h1 className='h1 text-center lg:text-left mb-2'>
+                        <motion.h1 
+                            variants={fadeIn('up', 0.4)} 
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}} 
+                            className='h1 text-center lg:text-left mb-2'>
                             <span>Where hard</span> work meets success
-                        </h1>
-                        <p className='text-white italic text-center lg:text-left mb-4'>
+                        </motion.h1>
+                        <motion.p 
+                            variants={fadeIn('up', 0.6)} 
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}} 
+                            className='text-white italic text-center lg:text-left mb-4'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veritatis non amet.
-                        </p>
-                        <CustomButton
-                            text='Get Started'
-                            containerStyles='w-[196px] h-[62px]'
-                        />
+                        </motion.p>
+                        <motion.div
+                            variants={fadeIn('up', 0.8)}
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}}
+                        >
+                            <CustomButton
+                                text='Get Started'
+                                containerStyles='w-[196px] h-[62px]'
+                            />
+                        </motion.div>
                     </div> 
                 </div>
             </SwiperSlide>
+
+            <SwiperSlide>
+                <div className='h-full flex justify-end pt-48'>
+                    <div className='flex flex-col items-center lg:items-start lg:max-w-[850px]'>
+                        <motion.h1 
+                            variants={fadeIn('up', 0.4)} 
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}} 
+                            className='h1 text-center lg:text-left mb-2'>
+                            <span>Where hard</span> work meets success
+                        </motion.h1>
+                        <motion.p 
+                            variants={fadeIn('up', 0.6)} 
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}} 
+                            className='text-white italic text-center lg:text-left mb-4'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veritatis non amet.
+                        </motion.p>
+                        <motion.div
+                            variants={fadeIn('up', 0.8)}
+                            initial='hidden' 
+                            whileInView={'show'}
+                            viewport={{once: false, amount: 0.2}}
+                        >
+                            <CustomButton
+                                text='Get Started'
+                                containerStyles='w-[196px] h-[62px]'
+                            />
+                        </motion.div>
+                    </div> 
+                </div>
+            </SwiperSlide>
+
             <SwiperNavButtons 
                 containerStyles='absolute bottom-2 lg:bottom-0 right-0 h-[130px] 
                 w-full lg:w-[850px] z-50 flex justify-center lg:justify-start gap-1' 
